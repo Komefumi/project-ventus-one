1203437e1a73596c9eb4c674e2021dd33eb44f1d
Hafiz <komefumi@protonmail.ch>
2023-09-02 22:45:16 +0530

    Makes minimal theme

---
+++ page.php
@@ -0,0 +1,15 @@
+<?php
+
+get_header();
+
+echo "<h1>This is a Page, not a Post</h1>";
+
+while (have_posts()) {
+  the_post(); ?>
+  <h2><?php the_title(); ?></h2>
+  <?php the_content(); ?>
+<?php }
+
+get_footer();
+
+?>
