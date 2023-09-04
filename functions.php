3f205014439d85dc819b90e3384bc630a9c3738f
Hafiz <komefumi@protonmail.ch>
2023-09-04 08:02:14 +0530

    Adds custom menus

--- functions.php
+++ functions.php
@@ -21,6 +21,9 @@ add_action('wp_enqueue_scripts', 'university_files');
 
 function university_features()
 {
+  register_nav_menu('header_menu_location', 'Header Menu Location');
+  register_nav_menu('footer_location_1', 'Footer Location 1');
+  register_nav_menu('footer_location_2', 'Footer Location 2');
   add_theme_support('title-tag');
 }
 
