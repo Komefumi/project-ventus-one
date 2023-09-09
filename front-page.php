3cc248f6c3799e26e1223962047f5514c5f0e7fd
Hafiz <komefumi@protonmail.ch>
2023-09-10 02:36:11 +0530

    Shows 2 ordered events in front page Shows excerpt when available

--- front-page.php
+++ front-page.php
@@ -16,28 +16,47 @@
       <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
       <?php
+      $today = date('Ymd');
       $homepage_events = new WP_Query(array(
         'posts_per_page' => 2,
         'post_type' => 'event',
+        'meta_key' => 'event_date',
+        'orderby' => 'meta_value_num',
+        'order' => 'asc',
+        'meta_query' => array(
+          array(
+            'key' => 'event_date',
+            'compare' => '>=',
+            'value' => $today,
+            'type' => 'numeric',
+          ),
+        ),
       ));
 
       while ($homepage_events->have_posts()) {
         $homepage_events->the_post(); ?>
         <div class="event-summary">
           <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-            <span class="event-summary__month"><?php the_time('M'); ?></span>
-            <span class="event-summary__day"><?php the_time('d'); ?></span>
+            <?php
+            $event_date = new DateTime(get_field('event_date'));
+            ?>
+            <span class="event-summary__month">
+              <?php
+              echo $event_date->format('M');
+              ?>
+            </span>
+            <span class="event-summary__day"><?php echo $event_date->format('d') ?></span>
           </a>
           <div class="event-summary__content">
             <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-            <p><?php echo wp_trim_words(get_the_content(), 18); ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+            <p><?php echo has_excerpt() ? get_the_excerpt() : wp_trim_words(get_the_content(), 18) ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
           </div>
         </div>
       <?php }
       wp_reset_postdata();
       ?>
 
-      <p class="t-center no-margin"><a href="<?php echo site_url('/events'); ?>" class="btn btn--blue">View All Events</a></p>
+      <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event') ?>" class="btn btn--blue">View All Events</a></p>
     </div>
   </div>
   <div class="full-width-split__two">
@@ -56,7 +75,7 @@
           </a>
           <div class="event-summary__content">
             <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-            <p><?php echo wp_trim_words(get_the_content(), 18); ?><a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+            <p><?php echo has_excerpt() ? get_the_excerpt() : wp_trim_words(get_the_content(), 18) ?><a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
           </div>
         </div>
       <?php }
@@ -65,7 +84,7 @@
 
 
 
-      <p class="t-center no-margin"><a href="<?php echo site_url('/blog'); ?>" class="btn btn--yellow">View All Blog Posts</a></p>
+      <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('post') ?>" class="btn btn--yellow">View All Blog Posts</a></p>
     </div>
   </div>
 </div>
