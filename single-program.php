3ce201bc50a0b525d8cfc748cd8b80c439d3fe20
Hafiz <komefumi@protonmail.ch>
2023-09-11 00:06:16 +0530

    Conditionally shows upcoming events

--- single-program.php
+++ single-program.php
@@ -48,31 +48,37 @@ while (have_posts()) {
         ),
       ),
     ));
-    while ($homepage_events->have_posts()) {
-      $homepage_events->the_post(); ?>
-      <div class="event-summary">
-        <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-          <?php
-          $event_date = new DateTime(get_field('event_date'));
-          ?>
-          <span class="event-summary__month">
-            <?php
-            echo $event_date->format('M');
-            ?>
-          </span>
-          <span class="event-summary__day"><?php echo $event_date->format('d') ?></span>
-        </a>
-        <div class="event-summary__content">
-          <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-          <p><?php echo has_excerpt() ? get_the_excerpt() : wp_trim_words(get_the_content(), 18) ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-        </div>
-      </div>
-    <?php }
-    wp_reset_postdata();
     ?>
+    <?php if ($homepage_events->have_posts()) { ?>
+      <hr class="section-break">
+      <h2 class="headline headline--medium">Upcoming <?php echo get_the_title(); ?> Events</h2>
+      <?php
+      while ($homepage_events->have_posts()) {
+        $homepage_events->the_post(); ?>
+        <div class="event-summary">
+          <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+            <?php
+            $event_date = new DateTime(get_field('event_date'));
+            ?>
+            <span class="event-summary__month">
+              <?php
+              echo $event_date->format('M');
+              ?>
+            </span>
+            <span class="event-summary__day"><?php echo $event_date->format('d') ?></span>
+          </a>
+          <div class="event-summary__content">
+            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+            <p><?php echo has_excerpt() ? get_the_excerpt() : wp_trim_words(get_the_content(), 18) ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+          </div>
+        </div>
+      <?php }
+      wp_reset_postdata();
+      ?>
   </div>
 <?php }
+  }
 
-get_footer();
+  get_footer();
 
 ?>
